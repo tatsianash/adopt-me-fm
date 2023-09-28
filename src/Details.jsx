36942133d20a665dd1423147dda88a9595b12167
Tatsiana Sh <tatsiana15az@gmail.com>
2023-09-28 14:48:52 -0700

    add error boundary

--- src/Details.jsx
+++ src/Details.jsx
@@ -3,6 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 const Details = () => {
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
@@ -30,4 +31,11 @@ const Details = () => {
   );
 };
 
-export default Details;
+function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
+export default DetailsErrorBoundary;
