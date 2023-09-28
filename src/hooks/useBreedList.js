68fcfd2412f0d7d452a130b1d58db3ad951aa7c2
Tatsiana Sh <tatsiana15az@gmail.com>
2023-09-28 16:55:26 -0700

    Basic structure

--- src/hooks/useBreedList.js
+++ src/hooks/useBreedList.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import fetchBreedList from "./fetchBreedList";
+import fetchBreedList from "../api/fetchBreedList";
 
 const useBreedList = (animal) => {
   const results = useQuery(["breeds", animal], fetchBreedList);
